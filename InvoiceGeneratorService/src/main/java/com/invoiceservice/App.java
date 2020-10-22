b749b5b13f4783dbf9e5fad36d3bd25952aa7f79
harshit031196 <guptaharshit0311@gmail.com>
2020-10-22 23:17:38 +0530

    Added the first commit

---
+++ InvoiceGeneratorService/src/main/java/com/invoiceservice/App.java
@@ -0,0 +1,13 @@
+package com.invoiceservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
